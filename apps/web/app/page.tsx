6806f2539f0aa5b911d3ccf27f439d43eb432f10
Ashwin <ashwin.2201098cs@iiitbh.ac.in>
2025-03-16 21:50:44 +0530

    initialize project structure with TypeScript, Tailwind CSS, and ESLint configurations

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Card } from '@repo/ui/card'
+function Page() {
+  return (
+    <div className='text-2xl '>
+      <Card />
+    </div>
+  )
+}
+
+export default Page
---
+++ packages/ui/src/card.tsx
@@ -0,0 +1,9 @@
+import { type ReactNode } from "react";
+import { Button } from "antd";
+export function Card() {
+  return (
+    <div className="">
+      <Button type="primary">Button</Button>
+    </div>
+  );
+}
