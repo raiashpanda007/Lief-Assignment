2c1463e4a6e61c38a720d847a67ab7fda803955e
Ashwin <ashwin.2201098cs@iiitbh.ac.in>
2025-03-17 08:23:51 +0530

    feat: add NextAuth configuration with Google provider and update dependencies

---
+++ apps/web/lib/Auth.ts
@@ -0,0 +1,13 @@
+import { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+const NEXT_AUTH_OPTIONS: NextAuthOptions = {
+    providers: [
+        GoogleProvider({
+            clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || "",
+            clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET || "",
+        }),
+    ],
+    secret: process.env.NEXT_PUBLIC_SECRET,
+    
+};
